9f3bde74c7e065ed3e558b5e6402da2ced2393cb
DaphRz <159149963+DaphRz@users.noreply.github.com>
2026-03-09 10:34:53 -0300

    Google Drive

---
+++ Desenvolvimento Web/UVV-1/tasks.py
@@ -0,0 +1,10 @@
+from invoke import task
+import upload_drive
+
+
+@task
+def upload_zip(c, file="arquivo.zip"):
+    """
+    Envia um arquivo zipado para o Google Drive.
+    """
+    upload_drive.upload_to_drive(file_path=file)
